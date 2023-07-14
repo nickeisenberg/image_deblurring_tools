2ff97f9ff0d0d97a5a05eb1e4f1c1e8ef1241444
Nick Eisenberg <nickeisenberg@gmail.com>
2023-07-13 17:09:39 -0700

    add_init feature to the model class

--- regressors.py
+++ regressors.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.special import erf as _erf
+from copy import deepcopy
 
 class Erf:
     def __init__(self, a=1, b=1):
@@ -45,6 +46,7 @@ class Model:
         self.reg = {}
         self.reg_counts = {}
         self.weights = {}
+        self.weights_vec = np.array([])
 
     def add(self, count, reg_class, reg_name):
         """
@@ -64,7 +66,41 @@ class Model:
             total = np.array([*self.reg_counts.values()]).sum()
             for rt, count in self.reg_counts.items():
                 self.weights[rt] = np.ones((count, 2)) * [1 / total, 1]
+                self.weights_vec = np.vstack(
+                    [*self.weights.values()]
+                ).reshape(-1)
 
+        else:
+            self.weights = weights
+            self.weights_vec = np.vstack(
+                [*weights.values()]
+            ).reshape(-1)
+
+    def add_initialize(self, count, weights, reg_class, reg_name):
+        """
+        reg_type: erf, arctan, bennett, custom
+        """
+
+        self.reg[reg_name] = reg_class
+
+        try:
+            self.reg_counts[reg_name] += count
+        except:
+            self.reg_counts[reg_name] = count
+
+        try:
+            self.weights[reg_name] = np.vstack(
+                (self.weights[reg_name], weights)
+            )
+        except:
+            self.weights[reg_name] = weights
+
+        try:
+            self.weights_vec = np.hstack(
+                (self.weights_vec, weights.reshape(-1))
+            )
+        except:
+            self.weights_vec = weights.reshape(-1)
 
     def esf(self, x):
         esf_ = 0 
@@ -86,13 +122,14 @@ class Model:
 
 # Testing the classes
 
+#--------------------------------------------------
 model = Model()
 
-model.add(3,  Bennett, 'bennett',)
-model.add(2, Erf, 'erf')
-model.add(10, Arctan, 'arctan')
+erf_weights = np.array([[.25, 1], [.25, 1]])
+model.add_initialize(2, erf_weights, Erf, 'erf')
 
-model.initialize()
+at_weights = np.array([[.25, 1], [.25, 1]])
+model.add_initialize(2, at_weights, Arctan, 'arctan')
 
 model_esf = model.esf(np.linspace(-10, 10, 1000))
 model_psf = model.psf(np.linspace(-10, 10, 1000))
@@ -100,3 +137,24 @@ model_psf = model.psf(np.linspace(-10, 10, 1000))
 plt.plot(model_psf)
 plt.plot(model_esf)
 plt.show()
+#--------------------------------------------------
+
+#--------------------------------------------------
+model = Model()
+
+model.add(2, Erf, 'erf')
+model.add(2, Arctan, 'arctan')
+
+weights = {
+    'erf': np.array([[2, 1], [1, 1]]),
+    'arctan': np.array([[.2, 1], [2, 22]]),
+}
+model.initialize(weights=weights)
+
+model_esf = model.esf(np.linspace(-10, 10, 1000))
+model_psf = model.psf(np.linspace(-10, 10, 1000))
+
+plt.plot(model_psf)
+plt.plot(model_esf)
+plt.show()
+#--------------------------------------------------
